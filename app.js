573349ba7132e6b75660a0964f29947e2e6e1e31
Shreyansh shah <shreyanshshah242@gmail.com>
2021-04-21 13:43:55 +0530

    commit

--- app.js
+++ app.js
@@ -114,6 +114,40 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This route is not yet defined',
+  });
+};
 //app.get('/api/v1/tours', getAllTours);
 // app.get('/api/v1/tours/:id', getTour);
 //app.post('/api/v1/tours', createTour);
@@ -130,7 +164,13 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
-app.route('/api/v1/users').get(getAllUsers);
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 // 4. STARTING THE SERVER
 
