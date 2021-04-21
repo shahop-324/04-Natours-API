e4622a633deea4bfb61767448e1c192ab42a69bf
Shreyansh shah <shreyanshshah242@gmail.com>
2021-04-21 16:57:00 +0530

    commit

--- Routes/tourRoutes.js
+++ Routes/tourRoutes.js
@@ -6,10 +6,15 @@ const router = express.Router();
 
 router.param('id', tourController.checkID);
 
+// Create a checkBody middleware
+// Check if body contains the name and price property
+// If not, send back 400
+// Add it to the post handler stack
+
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 
 router
   .route('/:id')
--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(morgan('dev'));
 
 app.use(express.json()); // Middleware // use method is used to add middlewares to our middleware stack
+app.use(express.static(`${__dirname}/public`));
+
 
 // Remember that in each middleware we have access to request and response objects.
 // We also have access to a special function next() to call next middleware in our middleware stack
@@ -26,13 +28,9 @@ app.use((req, res, next) => {
   next();
 });
 
-
-
 // 3. ROUTES
 
 app.use('/api/v1/tours', tourRouter); // Middleware used only on tours resource
 app.use('/api/v1/users', userRouter); // Middleware used only on users resource
-
-
 
 module.exports = app;
