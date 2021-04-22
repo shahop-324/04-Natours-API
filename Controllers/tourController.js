ae021156ee521c3c8bf308cec3659dac6fb2342f
Shreyansh shah <shreyanshshah242@gmail.com>
2021-04-22 22:15:29 +0530

    commit

--- Controllers/tourController.js
+++ Controllers/tourController.js
@@ -9,12 +9,12 @@ exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
     // BUILD QUERY
-    // 1) Filtering
+    // 1A) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 2) Advance Filtering
+    // 1B) Advance Filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
@@ -22,7 +22,16 @@ exports.getAllTours = async (req, res) => {
     // {difficulty: 'easy}, duration: { $gte: 5 } }
     // gte, gt, lte, lt
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+      query = query.sort(sortBy);
+      // sort('price ratingAverage')
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
