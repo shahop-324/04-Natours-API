acc5166a7acb712d8c4956e2fc79c759fb452b13
Shreyansh shah <shreyanshshah242@gmail.com>
2021-04-24 20:24:42 +0530

    commit

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,5 +1,15 @@
+/* eslint-disable no-unused-vars */
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newobj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newobj[el] = obj[el];
+  });
+  return newobj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
@@ -10,6 +20,31 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     results: users.length,
     data: {
       users: users,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create Error if user Posts password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+  // 2) Update user document
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
     },
   });
 });
