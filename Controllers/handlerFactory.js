4eff060b20120f21f9e7e128bb8fa265416476e3
Shreyansh shah <shreyanshshah242@gmail.com>
2021-04-26 14:25:47 +0530

    commit

--- Controllers/handlerFactory.js
+++ Controllers/handlerFactory.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable prettier/prettier */
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
@@ -59,10 +60,13 @@ exports.getOne = (Model, populateOptions) => catchAsync(async (req, res, next) =
 });
 
 exports.getAll = Model => catchAsync(async (req, res, next) => {
-  // eslint-disable-next-line no-console
+  // To allow for nested GET reviews on tour
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
 
+  
   // EXECUTE QUERY
-  const features = new APIFeatures(Model.find(), req.query)
+  const features = new APIFeatures(Model.find(filter), req.query)
     .filter()
     .sort()
     .limitFields()
