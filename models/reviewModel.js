9be034c0565fd98f253ec30e3fcd668349c12918
Shreyansh shah <shreyanshshah242@gmail.com>
2021-05-01 10:27:30 +0530

    commit

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -35,6 +35,8 @@ const reviewSchema = new mongoose.Schema(
   { toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   //   this.populate({ path: 'tour', select: 'name' }).populate({
   //     path: 'user',
